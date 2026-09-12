dc40d92a7b15869854d151bdff75695bec5cb6d9
asifa <arshiyaanjum0103@gmail.com>
2026-09-12 20:29:50 +0530

    wmp script

---
+++ main.tf
@@ -0,0 +1,17 @@
+module "databases" {
+  for_each = var.databases
+  source   = "./modules/rds"
+
+  env        = var.env
+  subnet_ids = var.subnets
+
+  allocated_storage = each.value["allocated_storage"]
+}
+
+module "eks" {
+  source = "./modules/eks"
+
+  env        = var.env
+  subnets    = var.subnets
+
+}
